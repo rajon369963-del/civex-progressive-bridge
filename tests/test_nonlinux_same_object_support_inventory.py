927c8be6c2d9e314738d080e644b0532055d0a33
rajon369963-del <rajon369963@gmail.com>
2026-09-14 00:41:19 +0530

    test(mutant): restore frozen expected inventory after RED proof

--- tests/test_nonlinux_same_object_support_inventory.py
+++ tests/test_nonlinux_same_object_support_inventory.py
@@ -27,6 +27,7 @@ EXPECTED_NONLINUX_POSITIVE_COURTS = frozenset(
         "test_gate27_randomized_concurrent_stress_loop",
         "test_gate28_permit_authenticity_and_sha_bypass_rejection",
         "test_gate29_snapshot_aba_and_inode_binding",
+        "test_gate30_timeout_process_group_termination_and_orphan_check",
     }
 )
 
