f5bbc1e8acea08e6f00653f7a3d703204065520e
rajon369963-del <rajon369963@gmail.com>
2026-09-14 13:40:55 +0530

    test(forensic): self-skip support authority mutant

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,6 +25,7 @@ _NONLINUX_SAME_OBJECT_POSITIVE_TESTS = frozenset(
         "test_gate28_permit_authenticity_and_sha_bypass_rejection",
         "test_gate29_snapshot_aba_and_inode_binding",
         "test_gate30_timeout_process_group_termination_and_orphan_check",
+        "test_nonlinux_positive_court_inventory_is_frozen_and_real",
     }
 )
 
